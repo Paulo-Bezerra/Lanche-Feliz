912acbe4ccddc2dc87ce65b7dd66a182bd019241
Paulo-Bezerra <paulobez33@gmail.com>
2018-11-21 17:29:56 -0300

    Adição da classe com os atributos e metodos do cliente (nescessario para fazer a

---
+++ java/LancheFeliz/src/pkg_telas/Cliente.java
@@ -0,0 +1,33 @@
+package pkg_telas;
+
+public class Cliente {
+    
+    String cpf;
+    String nome;
+    String email;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
