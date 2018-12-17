9760e1dc808e47ba3323e9b11e222637c1c3cdc9
mld-dezz <antoniolucas.eng@gmail.com>
2018-12-17 13:14:52 -0300

    SELECT dos mais escolhidos

--- mysql/BD_Lanche_Feliz.sql
+++ mysql/BD_Lanche_Feliz.sql
@@ -4,11 +4,12 @@ USE bar;
 
 CREATE TABLE alimento (  
 	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	decricao TEXT
+	descricao TEXT
 );
 
 CREATE TABLE cardapio (  
 	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	nome VARCHAR (30),
 	decricao TEXT
 );
 
@@ -25,7 +26,7 @@ CREATE TABLE cliente(
     nome VARCHAR (50) NOT NULL,
     email VARCHAR (50) NOT NULL,
     id_cardapio INT,
-	PRIMARY KEY (cpf, email),
+	PRIMARY KEY (cpf),
     FOREIGN KEY (id_cardapio) REFERENCES cardapio (id)
 );
 
@@ -36,3 +37,52 @@ SELECT * FROM alimento;
 SELECT * FROM alim_card;
 
 SELECT * FROM cliente;
+
+
+
+(SELECT id, count(id) from cliente inner join cardapio on cardapio.id = cliente.id_cardapio group by id order by count(id) desc  limit 5);
+
+insert into cardapio (id,nome) values 
+(default, 'macarronada'),
+(default, 'macarronad'),
+(default, 'macarrona'),
+(default, 'macarronadao'),
+(default, 'macarronadaoo'),
+(default, 'macarronadaooo'),
+(default, 'macarronadaooo'),
+(default, 'macarro'),
+(default, 'macarronaooo'),
+(default, 'macarronoo'),
+(default, 'macarronadooooo');
+
+
+insert into cliente (cpf, nome, email, id_cardapio) values
+('168-852-653.07', 'robeval', 'robsonhhhhh', 1),
+('458-456-653.55', 'mirian', 'lokjjjaaa', 2),
+('966-456-653.48', 'robeva', 'robsonhhhhh', 2),
+('168-111-653.07', 'robivall', 'robsonhhhhh', 3),
+('986-456-653.07', 'mara', 'robsonhhhhh', 3),
+('168-456-653.66', 'luqinha', 'robsonhhhhh', 3),
+('168-456-653.75', 'dick', 'robsonhhhhh', 4),
+('168-456-653.78', 'joca', 'robsonhhhhh', 4),
+('168-456-653.01', 'pedo', 'robsonhhhhh', 4),
+('168-456-653.09', 'josafa', 'robsonhhhhh', 4),
+('168-456-785.78', 'and', 'robsonhhhhh', 5),
+('168-456-653.85', 'lucinha', 'robsonhhhhh', 5),
+('168-456-653.96', 'romario', 'robsonhhhhh', 5),
+('168-456-584.55', 'ronaldinho', 'robsonhhhhh', 9),
+('168-986-963.99', 'garrinha', 'robsonhhhhh', 9),
+('168-786-653.77', 'pele', 'robsonhhhhh', 7),
+('168-123-653.17', 'cleyton', 'robsonhhhhh', 7),
+('152-456-653.87', 'whinderson', 'robsonhhhhh', 7),
+('168-456-653.38', 'mane', 'robsonhhhhh', 7),
+('168-456-653.97', 'jose', 'robsonhhhhh', 8),
+('168-456-653.87', 'gerold', 'robsonhhhhh', 6),
+('168-456-653.37', 'teobaldo', 'robsonhhhhh', 10),
+('455-456-653.07', 'vadalco', 'robsonhhhhh', 8),
+('999-456-653.07', 'thomas', 'robsonhhhhh', 9),
+('102-456-653.07', 'juliclei', 'robsonhhhhh', 4),
+('486-456-653.07', 'jubileu', 'robsonhhhhh', 5),
+('111-111-111.11', 'irineu', 'irineu@doce', 11);
+
+drop table cliente;
