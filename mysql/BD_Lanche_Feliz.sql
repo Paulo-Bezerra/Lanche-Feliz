d973186ffa31f4747b081850495fc4e22ef85f18
mld-dezz <antoniolucas.eng@gmail.com>
2019-01-22 19:34:58 -0300

    modificação no sql

--- mysql/BD_Lanche_Feliz.sql
+++ mysql/BD_Lanche_Feliz.sql
@@ -96,3 +96,9 @@ SELECT * FROM alim_card;
 SELECT * FROM cliente;
 
 SELECT cardapio.id, cardapio.nome, cardapio.descricao, selecao.quantidade FROM cardapio, (SELECT id, COUNT(id) AS quantidade FROM cliente INNER JOIN cardapio ON cardapio.id = cliente.id_cardapio GROUP BY id ORDER BY COUNT(id) DESC LIMIT  5) AS selecao WHERE cardapio.id = selecao.id;
+
+
+delete from cardapio where id = 9;
+
+
+SELECT id FROM cardapio;
